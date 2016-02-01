5fa5ef012e2c0ce612551549771f67a28244c0b3
ShiroTakeda <shiro.takeda@gmail.com>
2016-02-01 13:42:44 +0900

    修正

--- run_compile_jecon.bat
+++ run_compile_jecon.bat
@@ -1,28 +1,24 @@
 
-if not exist .\pdf mkdir .\pdf
-
 @title 1st platex on %1
-call platex --kanji=utf8 ./%1.tex
+call platex --kanji=utf8 %1.tex
 
 @title pbibtex on %1
-call pbibtex --kanji=utf8 ./%1
+call pbibtex --kanji=utf8 %1
 
 @title 2nd platex on %1
-call platex --kanji=utf8 ./%1.tex
+call platex --kanji=utf8 %1.tex
 
 @title 3rd platex on %1
-call platex --kanji=utf8 ./%1.tex
+call platex --kanji=utf8 %1.tex
 
 @title dvipdfmx on %1
-call dvipdfmx -d 5 -f msembed.map %1
+call dvipdfmx -d 5 %1
 
-cp ./%1.pdf ./pdf
-
-rm ./%1.log
-rm ./%1.aux
-rm ./%1.blg
-rm ./%1.bbl
-rm ./%1.out
-rm ./%1.dvi
+: rm %1.log
+: rm %1.aux
+: rm %1.blg
+: rm %1.bbl
+: rm %1.out
+: rm %1.dvi
 
 exit
